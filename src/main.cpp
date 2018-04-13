8dbe61f4bdd3b090ea3fa04fe37822d0876a3278
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2018-04-12 22:59:07 -1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ map<uint256, set<uint256> > mapOrphanTransactionsByPrev;
 // Constant stuff for coinbase transactions we create:
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "GanjaProject Signed Message:\n";
+const string strMessageMagic = "CBD Signed Message:\n";
 
 std::set<uint256> setValidatedTx;
 
